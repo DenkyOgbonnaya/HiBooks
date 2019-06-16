cde5b146c09bbfce07b05a2a15a08df1c6db23bf
DenkyOgbonnaya <denkyogb@gmail.com>
2019-06-16 16:34:00 +0100

    add env example file and remove mlab url

--- server/model/database.js
+++ server/model/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config()
 
-const{DB_USER, DB_PASS} = process.env;
-const mongoDB = process.env.MONGODB_URI || `mongodb://${DB_USER}:${DB_PASS}@ds163694.mlab.com:63694/hibooks`; //'mongodb://localhost:27017/hellobooks'  
+const{MDB_URL} = process.env;
+const mongoDB = process.env.MONGODB_URI || MDB_URL; //'mongodb://localhost:27017/hellobooks'  
 
 const connectToDb = () => { 
     mongoose.connect(mongoDB)
